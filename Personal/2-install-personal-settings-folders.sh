6c848b4e149f8b7da02b54bcdbbe11fee20c30cf
Brett Crisp <brettcrisp2@gmail.com>
2024-01-15 13:02:08 +0700

    updating scripts

--- Personal/2-install-personal-settings-folders.sh
+++ Personal/2-install-personal-settings-folders.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 set -e
 
-# Close Thunar if it's running
-pkill thunar || true
-
 # Source and destination directories
 source_dir=~/minti3/personal-settings
 destination_dir=~/
 
-# Copy directories
-cp -r $source_dir/.bin-personal/ $destination_dir
-cp -r $source_dir/.config/ $destination_dir
-cp -r $source_dir/.local/ $destination_dir
-
 # Run chmod +x on *.sh files in .bin-personal
-chmod +x $destination_dir/.bin-personal/*.sh
+chmod +x $source_dir/.bin-personal/*.sh
+
+# Run chmod +x on all .sh files in .config/polybar and .local/bin/
+chmod +x $source_dir/.config/polybar/*.sh
+chmod +x $source_dir/.local/bin/*.sh
 
 # Run chmod +x on autotiling in .local directory
-chmod +x $destination_dir/.local/bin/autotiling
+chmod +x $source_dir/.local/bin/autotiling
 
-# Copy specific files
-cp $source_dir/.gtkrc-2.0.mine $destination_dir
-cp $source_dir/bash_aliases $destination_dir
+# Run chmod +x on .desktop files in .local/share/applications
+chmod +x $source_dir/.local/share/applications/*.desktop
 
-# Copy JPEG files to Pictures directory
-cp $source_dir/*.jpg ~/Pictures/
+# Copy directories with overwriting existing files
+cp -rf $source_dir/.bin-personal/ $destination_dir
+cp -rf $source_dir/.config/ $destination_dir
+cp -rf $source_dir/.local/ $destination_dir
 
+# Copy specific files with overwriting existing files
+cp -f $source_dir/.gtkrc-2.0.mine $destination_dir
+cp -f $source_dir/bash_aliases $destination_dir
+
+# Copy JPEG files to Pictures directory with overwriting existing files
+cp -f $source_dir/*.jpg ~/Pictures/
