882c397eb3ddaf96656b737a53d49ebd0a306587
Brett Crisp <brettcrisp2@gmail.com>
2024-01-16 08:52:44 +0700

    updating scripts

--- Personal/r-autotiling.sh
+++ Personal/r-autotiling.sh
@@ -7,11 +7,5 @@ sudo apt install python3-pip
 # Install autotiling using pip
 pip3 install autotiling --user
 
-# Create user-level bin directory if it doesn't exist
-mkdir -p $HOME/.local/bin
-
-# Run chmod +x on autotiling in user-level bin directory
-chmod +x $HOME/.local/bin/autotiling
-
 # Output a message
 echo "autotiling has been installed successfully!"
